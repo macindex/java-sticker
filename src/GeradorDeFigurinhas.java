30f3393eb1ebe2e2bfeddd112a9c58a09e92b322
macindex <machollanda@gmail.com>
2022-07-24 09:38:27 -0300

    classe gerador de figurinhas

---
+++ src/GeradorDeFigurinhas.java
@@ -0,0 +1,11 @@
+public class GeradorDeFigurinhas {
+    
+    public void cria(){
+        // Leitura da imagem
+        
+        // Cria nova imagem em memoria com transparencia com tamanho novo
+
+        // copiar a imagem original para nova imagem
+        
+    }
+}
